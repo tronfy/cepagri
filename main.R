0e66b2c4d294d0c110d9fc06a37a4d5ee485f124
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2021-08-26 09:59:21 -0300

    feat: remover entradas com erro

--- main.R
+++ main.R
@@ -1,11 +1,16 @@
+# ler dataset .csv
 names <- c("horario", "temp", "vento", "umid", "sensa")
 cepagri <- read.csv("cepagri.csv", header = FALSE, sep = ";", col.names = names)
 
+# extrair dataframe entre 2015-01-01 e 2020-12-31
 df <- data.frame(cepagri)
 df <- df[
   as.Date(df$horario, format = "%d/%m/%Y") >= "2015-01-01" &
     as.Date(df$horario, format = "%d/%m/%Y") <= "2020-12-31",
 ]
+
+# remover entradas com erro
+df <- na.omit(df)
 
 summary(df)
 head(df)
