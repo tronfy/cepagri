44760edae91e5abd764b3fdfa74d8f425c211920
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2021-08-26 09:47:35 -0300

    feat: gerar subset do intervalo entre datas

--- main.R
+++ main.R
@@ -1,4 +1,12 @@
-names <- c('horario', 'temp', 'vento', 'umid', 'sensa')
-cepagri <- read.csv('cepagri.csv', header = FALSE, sep = ';', col.names = names)
+names <- c("horario", "temp", "vento", "umid", "sensa")
+cepagri <- read.csv("cepagri.csv", header = FALSE, sep = ";", col.names = names)
 
-summary(cepagri)
+df <- data.frame(cepagri)
+df <- df[
+  as.Date(df$horario, format = "%d/%m/%Y") >= "2015-01-01" &
+    as.Date(df$horario, format = "%d/%m/%Y") <= "2020-12-31",
+]
+
+summary(df)
+head(df)
+tail(df)
